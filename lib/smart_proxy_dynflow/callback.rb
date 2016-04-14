ab1ae6e289ab6de43c991694b2135f9ca6e5cf77
Adam Ruzicka <aruzicka@redhat.com>
2016-04-14 10:56:38 +0200

    Fixes #14901 - Reuse Foreman's http request

--- lib/smart_proxy_dynflow/callback.rb
+++ lib/smart_proxy_dynflow/callback.rb
@@ -16,6 +16,27 @@ module Proxy
           self.new.callback(payload)
         end
       end
+
+      class Core < Proxy::HttpRequest::ForemanRequest
+        def uri
+          @uri ||= URI.parse Proxy::Dynflow::Plugin.settings.core_url
+        end
+
+        def relay(request)
+          path = request.env['REQUEST_PATH'].gsub(/^\/dynflow/, '/api')
+          req = case request.env['REQUEST_METHOD']
+          when 'GET'
+            request_factory.create_get path
+          when 'POST'
+            request_factory.create_post path, request.body.read
+          end
+          send_request req
+        end
+
+        def self.relay(request)
+          self.new.relay request
+        end
+      end
     end
   end
 end
--- lib/smart_proxy_dynflow/helpers.rb
+++ lib/smart_proxy_dynflow/helpers.rb
@@ -1,31 +1,10 @@
-require 'rest-client'
 module Proxy
   class Dynflow
     module Helpers
-      def relay_request(host = Proxy::Dynflow::Plugin.settings.core_url)
-        path = request.env['REQUEST_PATH'].gsub(/^\/dynflow/, '/api')
-        result = case request.env['REQUEST_METHOD']
-        when 'GET'
-          resource(host)[path].get
-        when 'POST'
-          resource(host)[path].post request.body.read
-        end
-        status result.code
-        body result.body
-      rescue RestClient::Exception => e
-        status e.http_code
-        body e.http_body
-      end
-
-      def headers
-        {
-          :content_type => :json,
-          :accept => :json
-        }
-      end
-
-      def resource(host)
-        @resource ||= RestClient::Resource.new(host, :headers => headers)
+      def relay_request
+        response = Proxy::Dynflow::Callback::Core.relay(request)
+        status response.code
+        body response.body
       end
     end
   end
