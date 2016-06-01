e55e4075632093c83359e28cd9bafb21abc18ce5
Ivan Nečas <inecas@redhat.com>
2016-06-01 19:04:35 +0200

    Fixes #14901 - make sure to keep content type

--- lib/smart_proxy_dynflow/helpers.rb
+++ lib/smart_proxy_dynflow/helpers.rb
@@ -3,6 +3,7 @@ module Proxy
     module Helpers
       def relay_request(from = /^\/dynflow/, to = '')
         response = Proxy::Dynflow::Callback::Core.relay(request, from, to)
+        content_type response.content_type
         status response.code
         body response.body
       end
