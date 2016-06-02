f58c7c074d8dd5151e3df3257cc15125e7971bd4
Kahiru Aru <kahiru@openmailbox.org>
2016-06-02 21:22:22 +0200

    Fixes #14901 - Fix broken tests, part 3

--- test/core_test/test_helper.rb
+++ test/core_test/test_helper.rb
@@ -2,7 +2,7 @@ require 'minitest/autorun'
 
 ENV['RACK_ENV'] = 'test'
 
-$LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
+$LOAD_PATH << File.join(File.dirname(__FILE__), '..', '..', 'lib')
 require "mocha/setup"
 require "rack/test"
 
@@ -14,6 +14,3 @@ SmartProxyDynflowCore::Settings.instance.log_file = nil
 SmartProxyDynflowCore::Settings.instance.standalone = true
 WORLD = SmartProxyDynflowCore::Dynflow::Testing.create_world
 SmartProxyDynflowCore::Core.instance.world = WORLD
-
-logdir = File.join(File.dirname(__FILE__), '..', 'logs')
-FileUtils.mkdir_p(logdir) unless File.exist?(logdir)
