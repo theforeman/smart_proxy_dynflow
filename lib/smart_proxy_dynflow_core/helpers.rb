15447feeedd0523d78da8c1f13d9007552487bb0
adamruzicka <adamruzicka@users.noreply.github.com>
2016-09-08 20:41:35 +0200

    Fixes #15506 - Proper HTTP code on SSL error

--- lib/smart_proxy_dynflow_core/helpers.rb
+++ lib/smart_proxy_dynflow_core/helpers.rb
@@ -7,15 +7,13 @@ module SmartProxyDynflowCore
     def authorize_with_ssl_client
       if %w(yes on 1).include? request.env['HTTPS'].to_s
         if request.env['SSL_CLIENT_CERT'].to_s.empty?
-          status 403
           Log.instance.error "No client SSL certificate supplied"
-          halt MultiJson.dump(:error => "No client SSL certificate supplied")
+          halt 403, MultiJson.dump(:error => "No client SSL certificate supplied")
         else
           client_cert = OpenSSL::X509::Certificate.new(request.env['SSL_CLIENT_CERT'])
           unless SmartProxyDynflowCore::Core.instance.accepted_cert_serial == client_cert.serial
             Log.instance.error "SSL certificate with unexpected serial supplied"
-            halt MultiJson.dump(:error => "SSL certificate with unexpected serial supplied")
-            status 403
+            halt 403, MultiJson.dump(:error => "SSL certificate with unexpected serial supplied")
           end
         end
       else
