1d4e95d5a210315f443a8f4e272bf454a6cc7b21
Adam Ruzicka <aruzicka@redhat.com>
2017-09-01 10:52:24 -0400

    Fixes #20377 - Do auth properly

--- lib/smart_proxy_dynflow/api.rb
+++ lib/smart_proxy_dynflow/api.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'proxy/helpers'
+require 'sinatra/authorization'
 
 module Proxy
   class Dynflow
@@ -10,7 +11,13 @@ module Proxy
       before do
         logger = Proxy::LogBuffer::Decorator.instance
         content_type :json
+        if request.env['HTTP_AUTHORIZATION'] && request.env['PATH_INFO'].end_with?('/done')
+          # Halt running before callbacks if a token is provided and the request is notifying about task being done
+          return
+        end
       end
+
+      helpers Sinatra::Authorization
 
       post "/*" do
         relay_request
