236bf8389ed959c618b576f5bf967d165297f82b
Adam Ruzicka <aruzicka@redhat.com>
2017-09-20 10:59:23 +0200

    Fixes #20624 - Rename matchers to silencer_matchers

--- lib/smart_proxy_dynflow_core/core.rb
+++ lib/smart_proxy_dynflow_core/core.rb
@@ -44,7 +44,7 @@ module SmartProxyDynflowCore
         config.persistence_adapter = persistence_adapter
         config.execution_plan_cleaner = execution_plan_cleaner
         # TODO: There has to be a better way
-        matchers = config.silent_dead_letter_matchers.call().concat(self.class.plugin_matchers)
+        matchers = config.silent_dead_letter_matchers.call().concat(self.class.silencer_matchers)
         config.silent_dead_letter_matchers = matchers
         yield config if block_given?
       end
@@ -76,12 +76,12 @@ module SmartProxyDynflowCore
         @instance
       end
 
-      def plugin_matchers
+      def silencer_matchers
         @matchers ||= []
       end
 
-      def register_plugin_matchers(matchers)
-        plugin_matchers.concat matchers
+      def register_silencer_matchers(matchers)
+        silencer_matchers.concat matchers
       end
 
       def web_console
