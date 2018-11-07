8635da6a0cceeaea697f7745ffb470895e3fe439
Adam Ruzicka <aruzicka@redhat.com>
2018-11-07 15:43:41 +0100

    Fix Lint/IneffectiveAccessModifier cop

--- lib/smart_proxy_dynflow_core/callback.rb
+++ lib/smart_proxy_dynflow_core/callback.rb
@@ -10,6 +10,43 @@ end
 module SmartProxyDynflowCore
   module Callback
     class Request
+      class << self
+        def send_to_foreman_tasks(callback_info, data)
+          self.new.callback(self.prepare_payload(callback_info, data))
+        end
+
+        private
+
+        def prepare_payload(callback, data)
+          { :callback => callback, :data => data }.to_json
+        end
+
+        # rubocop:disable Metrics/PerceivedComplexity
+        def ssl_options
+          return @ssl_options if defined? @ssl_options
+          @ssl_options = {}
+          settings = Settings.instance
+          return @ssl_options unless URI.parse(settings.foreman_url).scheme == 'https'
+
+          @ssl_options[:verify_ssl] = OpenSSL::SSL::VERIFY_PEER
+
+          private_key_file = settings.foreman_ssl_key || settings.ssl_private_key
+          if private_key_file
+            private_key = File.read(private_key_file)
+            @ssl_options[:ssl_client_key] = OpenSSL::PKey::RSA.new(private_key)
+          end
+          certificate_file = settings.foreman_ssl_cert || settings.ssl_certificate
+          if certificate_file
+            certificate = File.read(certificate_file)
+            @ssl_options[:ssl_client_cert] = OpenSSL::X509::Certificate.new(certificate)
+          end
+          ca_file = settings.foreman_ssl_ca || settings.ssl_ca_file
+          @ssl_options[:ssl_ca_file] = ca_file if ca_file
+          @ssl_options
+        end
+        # rubocop:enable Metrics/PerceivedComplexity
+      end
+
       def callback(payload)
         response = callback_resource.post(payload, :content_type => :json)
         if response.code.to_s != "200"
@@ -18,45 +55,12 @@ module SmartProxyDynflowCore
         response
       end
 
-      def self.send_to_foreman_tasks(callback_info, data)
-        self.new.callback(self.prepare_payload(callback_info, data))
-      end
-
       private
-
-      def self.prepare_payload(callback, data)
-        { :callback => callback, :data => data }.to_json
-      end
 
       def callback_resource
         @resource ||= RestClient::Resource.new(Settings.instance.foreman_url + '/foreman_tasks/api/tasks/callback',
                                                self.class.ssl_options)
       end
-
-      # rubocop:disable Metrics/PerceivedComplexity
-      def self.ssl_options
-        return @ssl_options if defined? @ssl_options
-        @ssl_options = {}
-        settings = Settings.instance
-        return @ssl_options unless URI.parse(settings.foreman_url).scheme == 'https'
-
-        @ssl_options[:verify_ssl] = OpenSSL::SSL::VERIFY_PEER
-
-        private_key_file = settings.foreman_ssl_key || settings.ssl_private_key
-        if private_key_file
-          private_key = File.read(private_key_file)
-          @ssl_options[:ssl_client_key] = OpenSSL::PKey::RSA.new(private_key)
-        end
-        certificate_file = settings.foreman_ssl_cert || settings.ssl_certificate
-        if certificate_file
-          certificate = File.read(certificate_file)
-          @ssl_options[:ssl_client_cert] = OpenSSL::X509::Certificate.new(certificate)
-        end
-        ca_file = settings.foreman_ssl_ca || settings.ssl_ca_file
-        @ssl_options[:ssl_ca_file] = ca_file if ca_file
-        @ssl_options
-      end
-      # rubocop:enable Metrics/PerceivedComplexity
     end
 
     class Action < ::Dynflow::Action
