6a06c4020dea5212dee496f54bf9241b7b6b73ac
Adam Ruzicka <aruzicka@redhat.com>
2021-06-18 12:43:30 +0200

    Fixes #32836 - Fix OutputCollector superclass

--- lib/smart_proxy_dynflow/action/output_collector.rb
+++ lib/smart_proxy_dynflow/action/output_collector.rb
@@ -1,5 +1,5 @@
 module Proxy::Dynflow::Action
-  class OutputCollector < ::Proxy::Dynflow::Runner::Action
+  class OutputCollector < ::Proxy::Dynflow::Action::Runner
     def init_run
       output[:result] = []
       suspend
