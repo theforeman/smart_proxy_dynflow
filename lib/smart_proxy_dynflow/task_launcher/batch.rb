ed53d400dcf967710dbfd4a840c02036111e514b
Adam Ruzicka <aruzicka@redhat.com>
2021-07-08 08:23:21 +0200

    Fixes #32961 - Groundwork for async batch triggering

--- lib/smart_proxy_dynflow/task_launcher/batch.rb
+++ lib/smart_proxy_dynflow/task_launcher/batch.rb
@@ -2,35 +2,42 @@ module Proxy::Dynflow
   module TaskLauncher
     class Batch < Abstract
       def launch!(input)
-        trigger(nil, Proxy::Dynflow::Action::Batch, self, input)
+        plan = trigger(nil, action_class, self, input)
+        results[:parent] = format_result(plan)
       end
 
       def launch_children(parent, input_hash)
-        input_hash.each do |task_id, input|
+        input_hash.map do |task_id, input|
           launcher = child_launcher(parent)
-          launcher.launch!(transform_input(input))
+          triggered = launcher.launch!(transform_input(input), id: task_id)
           results[task_id] = launcher.results
+          triggered
         end
       end
 
       def prepare_batch(input_hash)
-        success_tasks = input_hash.select do |task_id, _input|
-          results[task_id][:result] == 'success'
-        end
-        success_tasks.reduce({}) do |acc, (key, value)|
-          acc.merge(results[key][:task_id] => value['action_input']['callback'])
-        end
+        input_hash
       end
-
-      private
 
       def child_launcher(parent)
         Single.new(world, callback, :parent => parent)
       end
 
+      private
+
       # Identity by default
       def transform_input(input)
         input
+      end
+
+      def action_class
+        Proxy::Dynflow::Action::Batch
+      end
+    end
+
+    class AsyncBatch < Batch
+      def action_class
+        Proxy::Dynflow::Action::AsyncBatch
       end
     end
   end
--- lib/smart_proxy_dynflow/task_launcher/single.rb
+++ lib/smart_proxy_dynflow/task_launcher/single.rb
@@ -5,10 +5,11 @@ module Proxy::Dynflow
         { :action_class => "MyActionClass", :action_input => {} }
       end
 
-      def launch!(input)
+      def launch!(input, id: nil)
         triggered = trigger(options[:parent],
                             action_class(input),
-                            with_callback(input.fetch('action_input', {})))
+                            with_callback(input.fetch('action_input', {})),
+                            id: id)
         @results = format_result(triggered)
         triggered
       end
