38d5d1e5fb771dc1ad71b90f0d3d5796fb06e803
Adam Ruzicka <aruzicka@redhat.com>
2021-07-09 13:17:59 +0200

    Fixes #32970 - Use ForemanRequest when calling back to Foreman

--- lib/smart_proxy_dynflow/callback.rb
+++ lib/smart_proxy_dynflow/callback.rb
@@ -2,56 +2,20 @@ require 'rest-client'
 
 module Proxy::Dynflow
   module Callback
-    class Request
-      class << self
-        def send_to_foreman_tasks(callback_info, data)
-          self.new.callback(prepare_payload(callback_info, data))
-        end
-
-        def ssl_options
-          return @ssl_options if defined? @ssl_options
-          @ssl_options = {}
-          settings = Proxy::SETTINGS
-          return @ssl_options unless URI.parse(settings.foreman_url).scheme == 'https'
-
-          @ssl_options[:verify_ssl] = OpenSSL::SSL::VERIFY_PEER
-
-          private_key_file = settings.foreman_ssl_key || settings.ssl_private_key
-          if private_key_file
-            private_key = File.read(private_key_file)
-            @ssl_options[:ssl_client_key] = OpenSSL::PKey::RSA.new(private_key)
-          end
-          certificate_file = settings.foreman_ssl_cert || settings.ssl_certificate
-          if certificate_file
-            certificate = File.read(certificate_file)
-            @ssl_options[:ssl_client_cert] = OpenSSL::X509::Certificate.new(certificate)
-          end
-          ca_file = settings.foreman_ssl_ca || settings.ssl_ca_file
-          @ssl_options[:ssl_ca_file] = ca_file if ca_file
-          @ssl_options
-        end
-        # rubocop:enable Metrics/PerceivedComplexity
-
-        private
-
-        def prepare_payload(callback, data)
-          { :callback => callback, :data => data }.to_json
-        end
+    class Request < ::Proxy::HttpRequest::ForemanRequest
+      def self.send_to_foreman_tasks(callback_info, data)
+        self.new.callback({ :callback => callback_info, :data => data }.to_json)
       end
 
       def callback(payload)
-        response = callback_resource.post(payload, :content_type => :json)
+        request = request_factory.create_post '/foreman_tasks/api/tasks/callback',
+                                              payload
+        response = send_request(request)
+
         if response.code.to_s != "200"
           raise "Failed performing callback to Foreman server: #{response.code} #{response.body}"
         end
         response
-      end
-
-      private
-
-      def callback_resource
-        @resource ||= RestClient::Resource.new(Proxy::SETTINGS.foreman_url + '/foreman_tasks/api/tasks/callback',
-                                               self.class.ssl_options)
       end
     end
 
