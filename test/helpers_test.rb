cf190b2f83c06f3daeede690cd9cc77a307f7818
archanaserver <ak31960466@gmail.com>
2023-05-16 15:31:17 +0530

    Fix Rubocop Minitest/AssertEmpty & Minitest/AssertEqual

--- test/helpers_test.rb
+++ test/helpers_test.rb
@@ -17,18 +17,18 @@ module Proxy::Dynflow
 
     it 'is not authenticated over HTTP' do
       get '/tasks/count', {}, {}
-      assert last_response.status == 200
+      assert_equal last_response.status, 200
     end
 
     it 'requires client SSL certificate when using https' do
       Log.instance.expects(:error)
       # HTTPS without client cert
       get '/tasks/count', {}, { 'HTTPS' => 'yes' }
-      assert last_response.status == 403
+      assert_equal last_response.status, 403
 
       # HTTPS with valid cert
       get '/tasks/count', {}, { 'HTTPS' => 'yes', 'SSL_CLIENT_CERT' => 'valid cert' }
-      assert last_response.status == 200
+      assert_equal last_response.status, 200
     end
 
     it 'performs token-based authentication for task update/done paths' do
@@ -40,27 +40,27 @@ module Proxy::Dynflow
       http_auth = "Basic #{::Proxy::Dynflow::OtpManager.tokenize(username, otp)}"
       Log.instance.stubs(:debug)
       post "/tasks/#{task_id}/update", '{}', 'HTTP_AUTHORIZATION' => http_auth
-      assert last_response.status == 200
+      assert_equal last_response.status, 200
 
       # Wrong password
       http_auth = "Basic #{::Proxy::Dynflow::OtpManager.tokenize(username, 'wrong pass')}"
       post "/tasks/#{task_id}/update", '{}', 'HTTP_AUTHORIZATION' => http_auth
-      assert last_response.status == 403
+      assert_equal last_response.status, 403
 
       # Wrong task id
       http_auth = "Basic #{::Proxy::Dynflow::OtpManager.tokenize(username, otp)}"
       post "/tasks/#{other_task_id}/update", '{}', 'HTTP_AUTHORIZATION' => http_auth
-      assert last_response.status == 403
+      assert_equal last_response.status, 403
 
       # Happy path for done
       http_auth = "Basic #{::Proxy::Dynflow::OtpManager.tokenize(username, otp)}"
       post "/tasks/#{task_id}/done", '{}', 'HTTP_AUTHORIZATION' => http_auth
-      assert last_response.status == 200
+      assert_equal last_response.status, 200
 
       # Call to done should remove the token, so using it the second time should fail
       http_auth = "Basic #{::Proxy::Dynflow::OtpManager.tokenize(username, otp)}"
       post "/tasks/#{task_id}/done", '{}', 'HTTP_AUTHORIZATION' => http_auth
-      assert last_response.status == 403
+      assert_equal last_response.status, 403
     end
   end
 end
