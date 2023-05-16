fedabd0937008c1f18577cb7e926b11a4d37eab3
archanaserver <ak31960466@gmail.com>
2023-05-16 15:08:35 +0530

    Fix Rubocop Style/SoleNestedConditional

--- lib/smart_proxy_dynflow/otp_manager.rb
+++ lib/smart_proxy_dynflow/otp_manager.rb
@@ -21,9 +21,10 @@ module Proxy::Dynflow
       def authenticate(hash, expected_user: nil, clear: true)
         plain = Base64.decode64(hash)
         username, otp = plain.split(':', 2)
-        if expected_user
-          return false unless expected_user == username
+        if expected_user && expected_user != username
+          return false
         end
+
         password_matches = passwords[username] == otp
         passwords.delete(username) if clear && password_matches
         password_matches
